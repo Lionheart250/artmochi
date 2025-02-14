b6e5596c21d6f0c5672a4d26d0e75d1c7142b1fb
Lionheart250 <jacksonlionheart@gmail.com>
2025-02-13 20:31:25 -0800

    readding offset to loading animation

--- client/src/components/LoadingSpirals.js
+++ client/src/components/LoadingSpirals.js
@@ -1,45 +1,50 @@
 import React, { useEffect, useRef, memo } from 'react';
 
 const LoadingSpirals = memo(() => {
-  const spiralRef = useRef(null);
-  const spiral2Ref = useRef(null);
-  const text = "Generating your image";
+  const containerRef = useRef(null);
+  const animationInitialized = useRef(false);
 
   useEffect(() => {
-    const setupSpiral = (ref, additionalOffset = 0) => {
-      if (!ref.current) return;
-      ref.current.innerHTML = '';
-      
-      const baseDelay = 500;
-      const duration = 2000;
-      const charCount = 16;
+    if (animationInitialized.current) return;
+    animationInitialized.current = true;
 
+    const text = "Generating your image";
+    const duration = 2000;
+    const baseOffset = 500;
+    const secondSpiralOffset = 4000;
+
+    const createSpiral = (offset) => {
+      const spiral = document.createElement('div');
+      spiral.className = 'spiral-text';
+      
       text.split('').forEach((char, i) => {
         const div = document.createElement('div');
         div.innerText = char;
-        div.classList.add('character');
+        div.className = 'character';
         div.style.setProperty('--i', i + 1);
-        div.style.animationDelay = `${-(baseDelay + additionalOffset) + (i * duration / charCount)}ms`;
-        ref.current.appendChild(div);
+        div.style.animationDelay = `${-(baseOffset + offset) + (i * duration / text.length)}ms`;
+        spiral.appendChild(div);
       });
+
+      return spiral;
     };
 
-    setupSpiral(spiralRef);
-    setupSpiral(spiral2Ref, 4000);
+    if (containerRef.current) {
+      containerRef.current.innerHTML = '';
+      containerRef.current.appendChild(createSpiral(0));
+      containerRef.current.appendChild(createSpiral(secondSpiralOffset));
+    }
 
     return () => {
-      if (spiralRef.current) spiralRef.current.innerHTML = '';
-      if (spiral2Ref.current) spiral2Ref.current.innerHTML = '';
+      animationInitialized.current = false;
+      if (containerRef.current) {
+        containerRef.current.innerHTML = '';
+      }
     };
   }, []);
 
-  return (
-    <div className="loading-text">
-      <div ref={spiralRef} className="spiral-text" />
-      <div ref={spiral2Ref} className="spiral-text" />
-    </div>
-  );
-});
+  return <div ref={containerRef} className="loading-text" />;
+}, () => true); // Force memo to always return true, preventing all re-renders
 
 LoadingSpirals.displayName = 'LoadingSpirals';
 
